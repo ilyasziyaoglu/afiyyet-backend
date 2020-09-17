827be9270193dce54bee269e7d7d8d647320e012
ILYAS ZIYAOGLU <ilyasziyaoglu@gmail.com>
2020-09-17 22:53:07 +0300

    getProductsByCategory eklendi

--- src/main/java/com/smartmenu/product/controller/ProductController.java
+++ src/main/java/com/smartmenu/product/controller/ProductController.java
@@ -3,11 +3,15 @@ package com.smartmenu.product.controller;
 import com.smartmenu.client.product.ProductRequest;
 import com.smartmenu.client.product.ProductResponse;
 import com.smartmenu.common.basemodel.controller.AbstractBaseController;
+import com.smartmenu.common.basemodel.service.ServiceResult;
 import com.smartmenu.product.db.entity.Product;
 import com.smartmenu.product.mapper.ProductMapper;
 import com.smartmenu.product.service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * @author Ilyas Ziyaoglu
@@ -32,5 +36,11 @@ public class ProductController extends AbstractBaseController<ProductRequest, Pr
 
 	public ProductMapper getMapper() {
 		return mapper;
+	}
+
+	@GetMapping("/get-products-by-category/{categoryId}")
+	public ResponseEntity<List<ProductResponse>> getProductsByCategory(@RequestHeader(HEADER_TOKEN) String token, @Valid @PathVariable Long categoryId) {
+		ServiceResult<List<Product>> serviceResult = getService().getProductsByCategory(token, categoryId);
+		return new ResponseEntity<>(getMapper().toResponse(serviceResult.getValue()), serviceResult.getHttpStatus());
 	}
 }
--- src/main/java/com/smartmenu/product/service/ProductService.java
+++ src/main/java/com/smartmenu/product/service/ProductService.java
@@ -3,12 +3,17 @@ package com.smartmenu.product.service;
 import com.smartmenu.client.product.ProductRequest;
 import com.smartmenu.client.product.ProductResponse;
 import com.smartmenu.common.basemodel.service.AbstractBaseService;
+import com.smartmenu.common.basemodel.service.ServiceResult;
 import com.smartmenu.product.db.entity.Product;
 import com.smartmenu.product.db.repository.ProductRepository;
 import com.smartmenu.product.mapper.ProductMapper;
 import com.smartmenu.product.mapper.ProductUpdateMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Ilyas Ziyaoglu
@@ -35,5 +40,19 @@ public class ProductService extends AbstractBaseService<ProductRequest, Product,
 	@Override
 	public ProductUpdateMapper getUpdateMapper() {
 		return updateMapper;
+	}
+
+	public ServiceResult<List<Product>> getProductsByCategory(String token, Long categoryId) {
+		ServiceResult<List<Product>> serviceResult = new ServiceResult<>();
+		try {
+			Optional<List<Product>> entityList = Optional.of(getRepository().findAllByCategoryId(categoryId));
+			serviceResult.setValue(entityList.get());
+			serviceResult.setHttpStatus(HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			serviceResult.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+			serviceResult.setMessage(e.getMessage());
+		}
+		return serviceResult;
 	}
 }
