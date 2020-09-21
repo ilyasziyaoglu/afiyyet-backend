40b4e8ec38a028f5eab2c0e4d3892ae1dccad25d
ILYAS ZIYAOGLU <ilyasziyaoglu@gmail.com>
2020-09-21 22:33:48 +0300

    sftp configuration

--- src/main/java/com/smartmenu/common/file/FileController.java
+++ src/main/java/com/smartmenu/common/file/FileController.java
@@ -23,7 +23,7 @@ public class FileController {
 
 	@PostMapping(value = "/upload")
 	public ResponseEntity<FileResponse> uploadFile(@RequestParam("file0") MultipartFile file) {
-		ServiceResult<String> serviceResult = service.uploadFileLocal(file);
+		ServiceResult<String> serviceResult = service.uploadFile(file);
 		return new ResponseEntity<>(new FileResponse(serviceResult.getValue()), serviceResult.getHttpStatus());
 	}
 
@@ -35,7 +35,7 @@ public class FileController {
 
 	@DeleteMapping(value = "/delete/{fileName}")
 	public ResponseEntity<Boolean> deleteFile(@PathVariable String fileName) {
-		ServiceResult<Boolean> serviceResult = service.deleteFileLocal(fileName);
+		ServiceResult<Boolean> serviceResult = service.deleteFile(fileName);
 		return new ResponseEntity<>(serviceResult.getValue(), serviceResult.getHttpStatus());
 	}
 
